e2f00022f5752e8c78b583688640bb9d82abd8a2
Andrei <avlugovov@gmail.com>
2023-06-28 12:31:00 +0400

    Update gendiff by step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,9 @@ program
   .name('gendiff')
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
