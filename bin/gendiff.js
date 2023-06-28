58253d8cadf95a383fd08bd8a3a6b35ae0738bb7
Andrei <avlugovov@gmail.com>
2023-06-28 12:18:58 +0400

    Update content by step 1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
