17eee9ad39badd0bdc1cda2d9b18ea44d334bbfe
Andrei <avlugovov@gmail.com>
2023-07-09 11:59:45 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,19 +4,13 @@ import path from 'path';
 
 const dataFile1 = readFileSync(path.resolve('__fixtures__', 'file1.json'), 'utf8');
 const dataFile2 = readFileSync(path.resolve('__fixtures__', 'file2.json'), 'utf8');
-const parsedDataFile1 = JSON.parse(getDataFile1);
-const parsedDataFile2 = JSON.parse(getDataFile2);
+const parsedDataFile1 = JSON.parse(dataFile1);
+const parsedDataFile2 = JSON.parse(dataFile2);
 
-console.log('parsedDataFile1:', parsedDataFile1)
-console.log('parsedDataFile2:', parsedDataFile2)
-console.log('____________')
-
-const gendiff = (obj1, obj2) => {
+const gendiffColl = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
+  const keys2 = Object.keys(obj1);
   const keys = _.union(keys1, keys2).sort();
-  console.log('keys:', keys);
-  console.log('____________');
   
   const result = keys.map((key) => {
     // if (!_.isPlainObject(key)) {
@@ -30,9 +24,26 @@ const gendiff = (obj1, obj2) => {
         return { key: key, value: obj1[key], status: 'unchanged' };
       }
     // }
-    // return { key: key, status: 'nested', children: gendiff(obj1[key], obj2[key]) };
+    // return { key: key, status: 'nested', children: gendiffColl(obj1[key], obj2[key]) };
   });
   return result;
 };
 
-export default gendiff;
+const renderTree = (array) => {
+  const tree = array.map(({ key, oldValue, value, status }) => {
+    switch (status) {
+      case 'added':
+        return `  +  ${key}: ${value}`;
+      case 'deleted':
+        return `  -  ${key}: ${value}`;
+      case 'changed':
+        return `  -  ${key}: ${oldValue}\n  +  ${key}: ${value}`;      
+      case 'unchanged':
+        return `     ${key}: ${value}`;
+    }
+  });
+  return `{\n${tree.join('\n')}\n}`;
+};
+
+const genDiff = () => renderTree(gendiffColl(parsedDataFile1, parsedDataFile2));
+export default genDiff;
