b434f04c565c1cb84dd800bfbf2f8f5e4503eeef
Andrei <avlugovov@gmail.com>
2023-07-09 17:12:40 +0400

    Update contebt by step 3

--- src/index.js
+++ src/index.js
@@ -1,49 +1,68 @@
-import { readFileSync } from 'fs';
 import _ from 'lodash';
+import { readFileSync } from 'fs';
 import path from 'path';
 
-const dataFile1 = readFileSync(path.resolve('__fixtures__', 'file1.json'), 'utf8');
-const dataFile2 = readFileSync(path.resolve('__fixtures__', 'file2.json'), 'utf8');
-const parsedDataFile1 = JSON.parse(dataFile1);
-const parsedDataFile2 = JSON.parse(dataFile2);
-
-const gendiffColl = (obj1, obj2) => {
+const makeDiffColl = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj1);
-  const keys = _.union(keys1, keys2).sort();
-  
-  const result = keys.map((key) => {
-    // if (!_.isPlainObject(key)) {
-      if (!Object.hasOwn(obj1, key)) {
-        return { key: key, value: obj2[key], status: 'added' };
-      } else if (!Object.hasOwn(obj2, key)) {
-        return { key: key, value: obj1[key], status: 'deleted' };
-      } else if (obj1[key] !== obj2[key]) {
-        return { key: key, value: obj1[key], status: 'changed' };
-      } else if (obj1[key] === obj2[key]) {
-        return { key: key, value: obj1[key], status: 'unchanged' };
-      }
-    // }
-    // return { key: key, status: 'nested', children: gendiffColl(obj1[key], obj2[key]) };
+  const keys2 = Object.keys(obj2);
+  const keys = _.sortBy(_.union(keys1, keys2));
+
+  const tree = keys.map((key) => {
+    if (!Object.hasOwn(obj1, key)) {
+      return { key, value: obj2[key], status: 'added' };
+    }
+    if (!Object.hasOwn(obj2, key)) {
+      return { key, value: obj1[key], status: 'deleted' };
+    }
+    if (obj1[key] !== obj2[key]) {
+      return { key, value: obj1[key], status: 'changed' };
+    }
+    if (obj1[key] === obj2[key]) {
+      return { key, value: obj1[key], status: 'unchanged' };
+    }
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+      return { key, children: makeDiffColl(obj1[key], obj2[key]), type: 'nested' };
+    }
+    return {
+      key, oldValue: obj1[key], newValue: obj2[key], type: 'changed',
+    };
   });
-  return result;
+  return tree;
 };
 
-const renderTree = (array) => {
-  const tree = array.map(({ key, oldValue, value, status }) => {
+const renderDiffTree = (array) => {
+  const tree = array.map(({
+    key, oldValue, value, status,
+  }) => {
     switch (status) {
       case 'added':
         return `  + ${key}: ${value}`;
       case 'deleted':
         return `  - ${key}: ${value}`;
       case 'changed':
-        return `  - ${key}: ${oldValue}\n  + ${key}: ${value}`;      
+        return `  - ${key}: ${oldValue}\n  + ${key}: ${value}`;
       case 'unchanged':
         return `    ${key}: ${value}`;
+      default:
+        throw new Error('Error');
     }
   });
   return `{\n${tree.join('\n')}\n}`;
 };
 
-const genDiff = () => renderTree(gendiffColl(parsedDataFile1, parsedDataFile2));
+const genDiff = (filepath1, filepath2) => {
+  const currentDirectory = process.cwd();
+  const resolvedFilepath1 = path.resolve(currentDirectory, filepath1);
+  const resolvedFilepath2 = path.resolve(currentDirectory, filepath2);
+
+  const dataFile1 = readFileSync(resolvedFilepath1, 'utf8');
+  const dataFile2 = readFileSync(resolvedFilepath2, 'utf8');
+  const parsedDataFile1 = JSON.parse(dataFile1);
+  const parsedDataFile2 = JSON.parse(dataFile2);
+
+  const diffColl = makeDiffColl(parsedDataFile1, parsedDataFile2);
+  const diffTree = renderDiffTree(diffColl);
+  return diffTree;
+};
+
 export default genDiff;
